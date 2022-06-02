b2e4a8487a902c60d830d8b2794bfcdfa8392d58
gototheTK <gototheTK>
2022-06-02 16:35:20 +0900

    portfolio

--- src/front-end/records/portfolio/freecodecamp/Freecodecamp.js
+++ src/front-end/records/portfolio/freecodecamp/Freecodecamp.js
@@ -15,7 +15,7 @@ class Freecodecamp extends Component {
             margin: "auto",
           }}
         >
-          <h1>Freecodecamp 직접 문제 풀어본것들</h1>
+          <h1>Freecodecamp 과제 프로젝트</h1>
           <hr />
         </div>
         <section id={"freecodecamp"} style={portfolioSection}>
