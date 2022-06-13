25baae290f8f8069df9dd3eb008dec533fc4a36f
gototheTK <gototkno1@gmail.com>
2022-06-14 04:46:21 +0900

    Double-Linked

--- src/front-end/records/cs/data_structures/Double-Linked-List.js
+++ src/front-end/records/cs/data_structures/Double-Linked-List.js
@@ -11,22 +11,28 @@ var DoublyLinkedList = function () {
   this.add = function (element) {
     const newNode = new Node(element, this.tail);
 
+    // 리스트가 비어있는지 확인한다.
     if (this.head) {
+      // 비어있지 않다면 테일의 next에 할당한다.
       this.tail.next = newNode;
       this.tail = newNode;
     } else {
+      // 비어있다면 헤더와 테일에 모두 값을 할당한다.
       this.head = newNode;
       this.tail = this.head;
     }
   };
 
   this.remove = function (element) {
+    // 리스트가 비어있는지 확인한다.
     if (this.head == null) return null;
 
+    // 헤더에 값이있으면 삭제한다.
     if (this.head.data === element) {
       this.head = this.head.next;
     }
 
+    // 꼬리에 값이 있으면 삭제한다.
     if (this.tail.data === element) {
       this.tail = this.tail.prev;
     }
@@ -34,6 +40,7 @@ var DoublyLinkedList = function () {
     let current = this.head;
     let previous;
 
+    // 리스트를 돌며 값이 있는 노드들을 삭제한다.
     while (current.next) {
       previous = current;
       current = current.next;
