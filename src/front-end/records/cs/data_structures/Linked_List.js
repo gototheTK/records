e75f742ebee7161d34098367404ebccfd706d0a0
gototheTK <gototkno1@gmail.com>
2022-06-14 03:52:34 +0900

    Linked List

--- src/front-end/records/cs/data_structures/Linked_List.js
+++ src/front-end/records/cs/data_structures/Linked_List.js
@@ -1,15 +1,19 @@
 function LinkedList() {
-  let length = 0;
-  let head = null;
+  var length = 0;
+  var head = null;
 
-  function Node(element) {
+  var Node = function (element) {
     this.element = element;
     this.next = null;
-  }
+  };
 
-  this.head = () => head;
+  this.size = function () {
+    return length;
+  };
 
-  this.size = () => length;
+  this.head = function () {
+    return head;
+  };
 
   this.add = function (element) {
     var node = new Node(element);
@@ -27,24 +31,44 @@ function LinkedList() {
     length++;
   };
 
-  this.remove = function (element) {
-    // Only change code below this line
-    if (head.element === element) {
-      head = head.next;
-      return length--;
+  // Only change code below this line
+  // 주어진 인덱스를 찾아 새로운 값의 노드를 넣는다.
+  this.addAt = function (index, element) {
+    // 이전 노드와 현재 노드를 가르키기 위한 변수 선언
+    let current = head;
+    let previous;
+    let newNode = new Node(element);
+
+    // 값을 넣고자하는 노드의 인덱스가 0일 경우
+    if (index === 0) {
+      newNode.index = 0;
+      newNode.next = head;
+      head = newNode;
+      length++;
+      return;
     }
-    let previous = head;
-    while (previous) {
-      let current = previous.next;
-      if (current) {
-        // makes sure we are not at end where current.next would be null
-        if (current.element === element) {
-          previous.next = current.next;
-          return length--;
-        }
-      }
+
+    // 값을 넣고자하는 인덱스가 1일 경우
+    while (index >= length && index > -1 && current.next) {
       previous = current;
+      current = current.next;
+
+      if (current.head === index) {
+        current.index++;
+
+        newNode.index = index;
+        newNode.next = current;
+
+        previous.next = newNode;
+
+        length++;
+
+        return;
+      }
     }
-    // Only change code above this line
+
+    return false;
   };
+
+  // Only change code above this line
 }
